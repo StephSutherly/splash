c383eed0126d6105ac4838b2cab0b460b58f88ba
Stephanie <stephanielarasutherland@gmail.com>
2023-01-10 14:43:13 +0000

    Add turtle icon; create basic splashscreen and home page

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # splash
 
 A Flutter project to have fun and create. This read me will expand/evolve as I progress with this application. 
+
+Turtle Icon by <a href="https://freeicons.io/profile/722">Fasil</a> on <a href="https://freeicons.io">freeicons.io</a>
+                                
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Center(
-        child: Text(
-          'Splash',
-          style: TextStyle(color: Colors.blue, decoration: TextDecoration.none),
-        ),
-      ),
+      home: SplashScreen(),
     ),
   );
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Center(child: Image.asset('assets/turtle_icon.png')),
+      nextScreen: const Home(),
+      backgroundColor: Colors.lightBlueAccent,
+      splashIconSize: 250.0,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+    );
+  }
 }
